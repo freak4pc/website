79ac44a1d346fa731b4abd78ad033f96167cf03b
Pedro Piñera <pepibumur@gmail.com>
2018-05-06 17:32:57 +0200

    Cache API responses

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,10 +3,42 @@ export default class Api {
     return Api.get('/teams/xcoders/members')
   }
   private static get(path: string): Promise<any> {
-    return fetch(`http://xcbuddy-website-api.herokuapp.com${path}`, {
+    return cachedFetch(`http://xcbuddy-website-api.herokuapp.com${path}`, {
       method: 'get',
-    }).then(res => {
-      return res.json()
     })
   }
+}
+
+const cachedFetch = (url, options) => {
+  return new Promise((resolve, reject) => {
+    let cachedResponse = sessionStorage.getItem(url)
+    if (cachedResponse) {
+      try {
+        let object = JSON.parse(cachedResponse)
+        if (
+          object.date &&
+          new Date(object.date * 1000 + 60 * 60 * 24) > new Date()
+        ) {
+          resolve(object.json)
+        } else {
+          reject('Expired')
+        }
+      } catch (error) {
+        reject(error)
+      }
+    } else {
+      reject('Not found')
+    }
+  }).catch(error => {
+    return fetch(url, options)
+      .then(res => res.json())
+      .then(json => {
+        let object = {
+          date: new Date().getTime() / 1000,
+          json: json,
+        }
+        sessionStorage.setItem(url, JSON.stringify(object))
+        return json
+      })
+  })
 }
